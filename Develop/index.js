807bba806e9ac9a5ac1f4855626c04afb450cb20
Gushihiro <robinsonhiroto@gmail.com>
2021-04-13 21:34:01 -0700

    set up structures

---
+++ Develop/index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+console.log(inquirer)
+console.log(fs)
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+//use inquirer
+//get the answers object
+//
+
+// Function call to initialize app
+init();
